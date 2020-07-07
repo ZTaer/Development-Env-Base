edcce0a54f09b9f0dc714d491ddd6a9708306c3f
ztaer <ztaerkiller@gmail.com>
2020-07-07 23:47:16 +0800

    react初始化配置，假如redux-saga请求超时写法

--- JavaScript-Developer/React-dev-env/Create-react-app-env/JavaScript-react-env/React-js-redux-base/src/redux/people/people.saga.js
+++ JavaScript-Developer/React-dev-env/Create-react-app-env/JavaScript-react-env/React-js-redux-base/src/redux/people/people.saga.js
@@ -1,4 +1,4 @@
-import { takeLatest, call, all, put, delay, select } from 'redux-saga/effects';
+import { takeLatest, call, all, put, select, race, delay } from 'redux-saga/effects';
 import { peopleActionTypes } from './people.types';
 import {
     fetchPeopleFailure,
@@ -10,6 +10,7 @@ import { selectPeopleOrigin } from './people.selectors';
 /**
  * 函数区
  */
+// 无超时写法
 export function* fetchPeopleReduxSagaStart(){
     try{
         const peopleDate = yield axios("https://swapi.py4e.com/api/people/");
@@ -20,14 +21,34 @@ export function* fetchPeopleReduxSagaStart(){
     catch(err){
         yield put( fetchPeopleFailure(err) );
     }
-}
+};
+// Redux Saga请求超时写法 - race超时写法( 等待笔记 )
+    // 0. 核心: race函数
+    // 1. 模型: const { posts, timeout } = yield race({ posts: 请求数据函数, timeout: delay(设定超时时间) });
+export function* fetchPeopleReduxSagaTimeOutStart(){
+    try{
+        const { posts, timeout } = yield race({
+            posts: axios("https://swapi.py4e.com/api/people/"),    // 请求数据
+            timeout: delay(1000)                                   // 超时时间设定1秒
+        });
+        if( posts.data ){
+            console.log("成功获取数据!");
+        }
+    }
+    catch(err){
+        console.log("访问超时!");
+    }
+};
 
 /**
  * 监听区
  */
 export function* onFetchPeopleReduxSagaStart() {
     yield takeLatest( peopleActionTypes.FETCH_PEOPLE_REDUX_SAGA_START,fetchPeopleReduxSagaStart );
-}
+};
+export function* onFetchPeopleReduxSagaTimeOutStart() {
+    yield takeLatest( peopleActionTypes.FETCH_PEOPLE_SUCCESS,fetchPeopleReduxSagaTimeOutStart );
+};
 
 /**
  * 执行区
@@ -35,5 +56,6 @@ export function* onFetchPeopleReduxSagaStart() {
 export function* peopleSaga(){
     yield all([
         call(onFetchPeopleReduxSagaStart),
+        call(onFetchPeopleReduxSagaTimeOutStart)
     ]);
 };
