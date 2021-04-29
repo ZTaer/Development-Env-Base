6afc2c4bc303b6d9360f2a091908845ac270baf4
ZTaer <ztaerkiller@gmail.com>
2021-04-30 00:07:36 +0800

    抽离create-react-app，去除ts强制校验

---
+++ JAVASCRIPT_DEV/ts-dev.react.create-react-app.webpack.redux.material-ui/src/components/test-text-lang/index.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { MainProps } from './index.model';
+
+const TestTextLang = (props: MainProps) => {
+
+  return (
+    <div>
+      <h1>test</h1>
+    </div>
+  );
+};
+
+export default TestTextLang;
